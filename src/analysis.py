bf2b9bb6dcce2f8416911dfec8ff1b9e94f1d6a2
rishabhsofficial <rishabhsofficial@gmail.com>
2025-03-17 00:51:13 +0530

    updated analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -7,12 +7,13 @@ from src.database import get_db_connection
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def get_top_rated_products():
-    """Retrieve top 3 rated products for each month and format month names."""
+    """Retrieve top 3 rated products for each month."""
     conn = get_db_connection()
     cursor = conn.cursor()
 
     logging.info("Fetching top-rated products for each month...")
 
+    # Used ROW_NUMBER() since only taking top 3 rates products for each month.
     cursor.execute("""
         SELECT month, product_id, avg_rating
         FROM (
@@ -20,16 +21,15 @@ def get_top_rated_products():
                 month,
                 product_id,
                 ROUND(avg_rating,2) as avg_rating,
-                RANK() OVER (PARTITION BY month ORDER BY avg_rating DESC) AS rank
+                ROW_NUMBER() OVER (PARTITION BY month ORDER BY avg_rating DESC) AS row_num
             FROM RatingsMonthlyAggregates
         ) 
-        WHERE rank <= 3;
+        WHERE row_num <= 3;
     """)
 
     results = cursor.fetchall()
     conn.close()
 
-    logging.info(f"Total top-rated records found: {len(results)}")
 
     # Convert YYYY-MM format to full month name
     formatted_results = []
